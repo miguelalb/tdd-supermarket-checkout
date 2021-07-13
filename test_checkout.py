e30608e90a1405e6f11ff5bae727edbbbfadd3fa
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-13 05:38:19 -0400

    Can calculate total

--- test_checkout.py
+++ test_checkout.py
@@ -9,5 +9,10 @@ def checkout():
 def test_CanAddItemPrice(checkout):
     checkout.addItemPrice("a", 1)
 
-def test_canAddItem(checkout):
+def test_CanAddItem(checkout):
     checkout.addItem("a")
+
+def test_CanCalculateTotal(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
