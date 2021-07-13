52eda74c3973fb8868990ce24b9277e3215a5339
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-13 05:42:23 -0400

    Can add multiple items

--- test_checkout.py
+++ test_checkout.py
@@ -6,13 +6,14 @@ def checkout():
     checkout = Checkout()
     return checkout
 
-def test_CanAddItemPrice(checkout):
-    checkout.addItemPrice("a", 1)
-
-def test_CanAddItem(checkout):
-    checkout.addItem("a")
-
 def test_CanCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
